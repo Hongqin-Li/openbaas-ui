418cb946454088e5464e4d7bb3177fea248302d3
Hongqin Li <ihongqinli@gmail.com>
2020-11-09 10:16:11 +0800

    chore: add github ci
    Change-Id: I7d68aae571868fca4f5a249284435f13704194e1

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,3 @@
-const path = require('path');
-
 module.exports = {
-  chainWebpack: config => {
-    config.module
-      .rule('eslint')
-      .use('eslint-loader')
-      .options({
-        fix: true,
-      });
-  },
-  configureWebpack: {
-    resolve: {
-      extensions: ['.js', '.vue', '.json', '.scss'],
-      alias: {
-        styles: path.resolve(__dirname, 'src/assets/scss'),
-      },
-    },
-  },
+  transpileDependencies: ["vuetify"]
 };
